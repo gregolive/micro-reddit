b6582eebb91efcb238336b258fcb1d0dc9f9fe61
Gregory Olive <gregoryolive@outlook.com>
2021-11-12 23:40:34 +0900

    Added comments model and validation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, length: { in: 4..40 }
   validates :url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
